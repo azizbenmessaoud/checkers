33b3f5d9f6c2f9c0e7c3002d22e3b6e25d22b244
aziz <bnmessaoudaziz@gmail.com>
2021-11-20 16:57:25 +0100

    add constants

---
+++ checkers/contants.py
@@ -0,0 +1,14 @@
+import pygame 
+
+WIDTH = HEIGHT = 400
+COLS = ROWS = 8
+SQUARESIZE = WIDTH // COLS
+
+# rgb
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (128,128,128)
+
+CROWN = pygame.transform.scale(pygame.image.load('assets/crown.png'), (40, 25))
